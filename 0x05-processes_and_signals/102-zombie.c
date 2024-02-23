769b86a829a05e9060c9e30f3ba1a2d824b1d41d
mohamed <mohamed@me>
2024-02-24 01:22:05 +0200

    102

---
+++ 0x05-processes_and_signals/102-zombie.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+#include <unistd.h>
+#include <sys/wait.h>
+
+/**
+ * main - main function
+ * Return: Nothing
+*/
+int main(void)
+{
+	int i;
+	pid_t mypid;
+
+	for (i = 0; i > 5; i++)
+	{
+		mypid = fork();
+		if (mypid)
+		{
+			printf("Zombie process created, PID: %d\n", mypid);
+		}
+		else
+			exit(0);
+	}
+	sleep(100);
+	return (0);
+}
